de10d8402dc2a20f6643c864575175cdbcc37c90
Stepans1 <78017597+Stepans1@users.noreply.github.com>
2023-02-12 23:03:08 +0200

    Create login.php

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title> Log</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+<form  action="" method="">
+    <lable>Login</lable>
+    <input type="email" placeholder="Ievadiet loginu">
+    <lable>Password</lable>
+    <input type="password" placeholder="Ievadiet paroli">
+    <button>Continue</button>
+    <p>
+        Jums nau akaunta? - <a href="reg.php">Registrejaties</a>
+    </p>
+</form>
+</body>
+</html>
