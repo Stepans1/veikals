cc67391714791a356c534febf8da643f5418c41d
Stepans Osipovs <78017597+Stepans1@users.noreply.github.com>
2023-02-16 21:53:45 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,9 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
+
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -6,14 +11,21 @@
     <link rel="stylesheet" href="main.css">
 </head>
 <body>
-<form  action="" method="">
+<form  action="signin.php" method="post">
     <lable>Login</lable>
-    <input type="email" placeholder="Ievadiet loginu">
+    <input type="email" name ="login" placeholder="Ievadiet loginu">
     <lable>Password</lable>
-    <input type="password" placeholder="Ievadiet paroli">
-    <button>Continue</button>
+    <input type="password" name="password" placeholder="Ievadiet paroli">
+    <button type="submit">Continue</button>
     <p>
         Jums nau akaunta? - <a href="reg.php">Registrejaties</a>
+    </p>
+    <p class="msg">
+        <?php
+
+        if(isset($_SESSION['done'])) { echo $_SESSION['done']; }
+        unset($_SESSION['done']);
+        ?>
     </p>
 </form>
 </body>
